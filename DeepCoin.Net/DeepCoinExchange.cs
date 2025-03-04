3aa80a429adc7a1ec73574ba5bdb2f948ba08b56
Jkorf <jankorf91@gmail.com>
2025-03-04 13:12:55 +0100

    Added Type property to DeepCoinExchange

--- DeepCoin.Net/DeepCoinExchange.cs
+++ DeepCoin.Net/DeepCoinExchange.cs
@@ -43,6 +43,11 @@ namespace DeepCoin.Net
             };
 
         /// <summary>
+        /// Type of exchange
+        /// </summary>
+        public static ExchangeType Type { get; } = ExchangeType.CEX;
+
+        /// <summary>
         /// Format a base and quote asset to an DeepCoin recognized symbol 
         /// </summary>
         /// <param name="baseAsset">Base asset</param>
