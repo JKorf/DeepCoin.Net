3eb6cc53ab60068b1e795e83b65bbf6e19a8c07c
Jkorf <jankorf91@gmail.com>
2025-03-04 13:13:24 +0100

    Updated DeepCoinOptions to derive from LibraryOptions

--- DeepCoin.Net/Objects/Options/DeepCoinOptions.cs
+++ DeepCoin.Net/Objects/Options/DeepCoinOptions.cs
@@ -1,4 +1,5 @@
 using CryptoExchange.Net.Authentication;
+using CryptoExchange.Net.Objects.Options;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -9,31 +10,7 @@ namespace DeepCoin.Net.Objects.Options
     /// <summary>
     /// DeepCoin options
     /// </summary>
-    public class DeepCoinOptions
+    public class DeepCoinOptions: LibraryOptions<DeepCoinRestOptions, DeepCoinSocketOptions, DeepCoinApiCredentials, DeepCoinEnvironment>
     {
-        /// <summary>
-        /// Rest client options
-        /// </summary>
-        public DeepCoinRestOptions Rest { get; set; } = new DeepCoinRestOptions();
-
-        /// <summary>
-        /// Socket client options
-        /// </summary>
-        public DeepCoinSocketOptions Socket { get; set; } = new DeepCoinSocketOptions();
-
-        /// <summary>
-        /// Trade environment. Contains info about URL's to use to connect to the API. Use `DeepCoinEnvironment` to swap environment, for example `Environment = DeepCoinEnvironment.Live`
-        /// </summary>
-        public DeepCoinEnvironment? Environment { get; set; }
-
-        /// <summary>
-        /// The api credentials used for signing requests.
-        /// </summary>
-        public DeepCoinApiCredentials? ApiCredentials { get; set; }
-
-        /// <summary>
-        /// The DI service lifetime for the IDeepCoinSocketClient
-        /// </summary>
-        public ServiceLifetime? SocketClientLifeTime { get; set; }
     }
 }
