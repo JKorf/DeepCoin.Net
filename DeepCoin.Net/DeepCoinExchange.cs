60ee0b3ffaad2a0f16d9671458cf5352ffa5d16a
JKorf <jankorf91@gmail.com>
2025-12-16 18:57:04 +0100

    Fix SharedSymbol.UsdOrStable mapping when using order book factory for spot asset

--- DeepCoin.Net/DeepCoinExchange.cs
+++ DeepCoin.Net/DeepCoinExchange.cs
@@ -88,7 +88,12 @@ namespace DeepCoin.Net
         public static string FormatWebsocketSymbol(string baseAsset, string quoteAsset, TradingMode tradingMode, DateTime? deliverTime = null)
         {
             if (tradingMode == TradingMode.Spot)
+            {
+                if (quoteAsset.Equals(SharedSymbol.UsdOrStable))
+                    quoteAsset = AssetAliases.CommonToExchangeName(SharedSymbol.UsdOrStable.ToUpperInvariant());
+
                 return baseAsset + "/" + quoteAsset;
+            }
 
             return baseAsset + quoteAsset;
         }
